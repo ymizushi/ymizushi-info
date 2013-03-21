60026000f32332632a3b5d11a58d88f1f76cfe3b
ymizushi <mizushi@gmail.com>
2013-03-22 01:42:44 +0900

    edit routing func

--- src/ymizushi_info/view/root.clj
+++ src/ymizushi_info/view/root.clj
@@ -1,11 +1,20 @@
 (ns ymizushi-info.view.root
   (:use hiccup.core))
 
-(defn header []
-  ())
+(defn contents []
+  (html [:html (str (head) (body) )])) 
+
+(defn head []
+  (html [:head
+         [:meta {:http-equiv "Content-Type" :content "text/html" :charset "shift_jis"}]
+         [:meta {:name "author" :content "ymizushi"}]
+         [:meta {:name "description" :content "clojure"}]
+         [:meta {:name "robots" :content "all"}]
+         [:title "ymizushi.info"]
+         ])) 
+
+(defn body []
+  (html [:body "body"])) 
 
 (defn index [x]
-  (html [:span {:class "index"} "index!"]))
-
-(defn get []
-  (html [:span {:class "get"} "gets!"]))
+  (contents))
