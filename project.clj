672be5f88e6bbf661777e5b2c3f1722d1803e6d3
ymizushi <mizushi@gmail.com>
2013-04-05 01:32:22 +0900

    add immutant

--- project.clj
+++ project.clj
@@ -4,8 +4,9 @@
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [compojure "1.1.5"]
                  [hiccup "1.0.2"]
-                 [mongoika "0.7.7"]]
+                 ]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler ymizushi-info.handler/app}
   :dev-dependencies [[ring/ring-devel "0.3.11"]]
-  :profiles {:dev {:dependencies [[ring-mock "0.1.3"]]}})
+  :profiles {:dev {:dependencies [[ring-mock "0.1.3"]]}}
+  :immutant {:context-path "/"})
