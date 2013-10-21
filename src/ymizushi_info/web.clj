410f7cc77476a6b10861484a39c61d4add5c461b
ymizushi <mizushi@gmail.com>
2013-10-21 23:50:59 +0900

    edit page

--- src/ymizushi_info/web.clj
+++ src/ymizushi_info/web.clj
@@ -4,7 +4,6 @@
             [compojure.route :as route]
             [clojure.java.io :as io]
             [ring.middleware.stacktrace :as trace]
-            [ring.middleware.session :as session]
             [ring.middleware.session.cookie :as cookie]
             [ring.adapter.jetty :as jetty]
             [ring.middleware.basic-authentication :as basic]
@@ -12,26 +11,8 @@
             [ymizushi-info.controller.profile :as profile]
             [ymizushi-info.controller.history :as history]
             [ymizushi-info.controller.downloads :as downloads]
-            [cemerick.drawbridge :as drawbridge]
             [environ.core :refer [env]])
   (:gen-class))
-
-(defn- authenticated? [user pass]
-  (= [user pass] [(env :repl-user false) (env :repl-password false)]))
-
-(def ^:private drawbridge
-  (-> (drawbridge/ring-handler)
-      (session/wrap-session)
-      (basic/wrap-basic-authentication authenticated?)))
-
-(defroutes app
-  (route/resources "/")
-  (ANY "/repl" {:as req} (drawbridge req))
-  (GET "/" [id] (root/action id))
-  (GET "/profile" [id] (profile/action id))
-  (GET "/history" [id] (history/action id))
-  (GET "/downloads" [id] (downloads/action id))
-  (ANY "*" [] (route/not-found (slurp (io/resource "404.html")))))
 
 (defn wrap-error-page [handler]
   (fn [req]
@@ -40,6 +21,14 @@
       {:status 500
        :headers {"Content-Type" "text/html"}
        :body (slurp (io/resource "500.html"))}))))
+
+(defroutes app
+  (route/resources "/")
+  (GET "/" [id] (root/action id))
+  (GET "/profile" [id] (profile/action id))
+  (GET "/history" [id] (history/action id))
+  (GET "/downloads" [id] (downloads/action id))
+  (ANY "*" [] (route/not-found (slurp (io/resource "404.html")))))
 
 (defn -main [& [port]]
   (let [port (Integer. (or port (env :port) 80))
