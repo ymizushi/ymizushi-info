da1125f00d9341c070c9208372eaf19e77fd669f
ymizushi <mizushi@gmail.com>
2013-12-14 04:10:07 +0900

    add test

--- test/ymizushi_info/test/view.clj
+++ test/ymizushi_info/test/view.clj
@@ -1,3 +1,16 @@
 (ns ymizushi-info.test.view
   (:require [clojure.test :refer :all]
             [ymizushi-info.view :refer :all]))
+
+(deftest test-table-render []
+  (testing "table-render"
+    (let [arg {:job ["Job" "Engineer"] :interest ["Intereset" "Do it yourself."]}
+          expected [:table {:class "table"}
+                     [:tr 
+                       [:td "Job" ] [:td "Engineer"]]
+                     [:tr
+                       [:td "Intereset"] [:td "Do it yourself."]]]]
+      (is
+        expected
+        (table-render arg)))))
+
