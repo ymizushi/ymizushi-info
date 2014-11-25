f2ebd3e06464afd55288f4ff2c2718c4112346ff
ymizushi <mizushi@gmail.com>
2014-11-26 04:13:06 +0900

    add resources

---
+++ resources/README.md
@@ -0,0 +1,4 @@
+resources
+=========
+
+the js/css/img resouce repository for submodule.
---
+++ resources/public/js/util.js
@@ -0,0 +1,5 @@
+function genUrl() {
+    var id = document.hoge.id.value;
+    document.users.action='http://localhost:5000/users/7';
+    document.users.submit();
+}
